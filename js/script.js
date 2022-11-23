1d618b8104d63bcaa0b3ee61f3f1f406a1c64e9f
Choi0619 <112284092+Choi0619@users.noreply.github.com>
2022-11-22 23:45:53 -0600

    add title in the fisrt page, javascript code containing google maps and font animation

---
+++ js/script.js
@@ -0,0 +1,43 @@
+// Google Map
+function initMap() {
+  var map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: 41.831353, lng: -87.628682 },
+    zoom: 14,
+  });
+  var marker = new google.maps.Marker({
+    position: { lat: 41.831353, lng: -87.628682 },
+    map: map,
+  });
+  var addCircle = new google.maps.Circle({
+    center: { lat: 41.831353, lng: -87.628682 },
+	radius:100,
+    strokeColor: "#00FF00",
+    strokeOpacity: 0.8,
+    strokeWeight: 2,
+    fillColor: "#00FF00",
+    fillOpacity: 0.5,
+	map: map,
+  });
+  var infowindow = new google.maps.InfoWindow({
+    content: "<p>This is the place where I live!</p>"
+  });
+  infowindow.open(map, marker);
+}
+window.initMap = initMap;
+
+
+// Font Animation
+var string = "Welcome to Gyuhwan Choi's Website!"; /* type your text here */
+var array = string.split("");
+var timer;
+
+function frameLooper () {
+	if (array.length > 0) {
+		document.getElementById("text").innerHTML += array.shift();
+	} else {
+		clearTimeout(timer);
+			}
+	loopTimer = setTimeout('frameLooper()',70); /* change 70 for speed */
+
+}
+frameLooper();
