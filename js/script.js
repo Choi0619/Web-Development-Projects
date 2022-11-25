c53e95d3c73351df08f6e52360f2f150f3d758a6
Gyuhwan Choi <112284092+Choi0619@users.noreply.github.com>
2022-11-25 01:45:50 -0600

    fix bug

--- js/script.js
+++ js/script.js
@@ -27,7 +27,7 @@ window.initMap = initMap;
 
 
 // Font Animation
-var string = "elcome to Gyuhwan Choi's Website!";
+var string = "Welcome to Gyuhwan Choi's Website!";
 var array = string.split("");
 var timer;
 
