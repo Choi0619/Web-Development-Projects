9312fb68f1680f887b01e0c75c513e9ad378206a
Gyuhwan Choi <112284092+Choi0619@users.noreply.github.com>
2022-11-25 01:27:08 -0600

    fix typo

--- js/script.js
+++ js/script.js
@@ -27,7 +27,7 @@ window.initMap = initMap;
 
 
 // Font Animation
-var string = "elcome to Gyuhwan Choi's Website!"; /* type your text here */
+var string = "lcome to Gyuhwan Choi's Website!";
 var array = string.split("");
 var timer;
 
